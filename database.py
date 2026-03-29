f272e55e02688007b1521cf43b3931f2b9f7278e
Akshat Gupta <akshat4899@gmail.com>
2026-03-30 02:34:35 +0530

    Solved hardcoded issue

--- database.py
+++ database.py
@@ -1,10 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+post_gres_db_name=os.getenv("POSTGRES_DB_NAME")
+post_gres_user=os.getenv("POSTGRES_USER")
+post_gres_password=os.getenv("POSTGRES_PASSWORD")
 
 engine = create_engine(
-    "postgresql+psycopg2://postgres:Akshat3422%40@localhost:5432/postgres",
-    echo=True,
+    f"postgresql+psycopg2://{post_gres_user}:{post_gres_password}@localhost:5432/{post_gres_db_name}"
 )
 
 def get_db():
